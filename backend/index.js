df95823d1e8507afcc99afa50025c90b8c392296
Vivek-kumar23501 <vivekkumarcse23501@gmail.com>
2026-09-16 08:28:38 +0530

    cors done

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
+    origin: ["http://localhost:5173", "https://upchar-project-dvca.vercel.app"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
   })
